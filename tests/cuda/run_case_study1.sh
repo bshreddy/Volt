7fc7d2baba8fa76f256e5674cfd963ba6dc50e87
Shinnung Jeong <sjeong306@gatech.edu>
2025-12-18 05:30:40 -0500

    minor update

--- tests/cuda/run_case_study1.sh
+++ tests/cuda/run_case_study1.sh
@@ -1,10 +1,11 @@
 AE_HOME_DIR=$(pwd)
 
 cd $CuPBoP_PATH/examples
+pwd
 
 #cd $VORTEX_PREFIX
 #./ci/blackbox.sh --cores=4 --warps=16 --threads=32 --l2cache --app=opencl/vecadd
 
-./CGO_batch_warp_feature_run.sh
+./CC_batch_warp_feature_run.sh
 
 cd $AE_HOME_DIR
